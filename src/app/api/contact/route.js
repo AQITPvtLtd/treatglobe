5310061fd466cc41bb0cd47acb5d095050d70b25
Palak <palaksehgal0707@gmail.com>
2024-05-28 11:46:26 +0530

    mail functionality fixed

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -65,6 +65,21 @@ export const POST = async (req, res) => {
       from: process.env.MY_EMAIL,
       to: process.env.MY_EMAIL,
       subject: "Treat Globe Contact form",
+      attachments: [
+        {
+          filename: medical,
+          path: path.join(
+            process.cwd(),
+            `public/documents`,
+            unique_id,
+            medical
+          ),
+        },
+        {
+          filename: iden,
+          path: path.join(process.cwd(), `public/documents`, unique_id, iden),
+        },
+      ],
       html: `<html lang="en">
           <head>
             <meta charset="utf-8">
@@ -93,7 +108,39 @@ export const POST = async (req, res) => {
           </body>
           </html>`,
     };
-    // transporter.sendMail(mailOptions);
+    transporter.sendMail(mailOptions);
+
+    //send confirmation mail to user
+    var mailOptions = {
+      from: process.env.MY_EMAIL,
+      to: email,
+      subject: "Thank You for contacting TreatGlobe!",
+      html: `<html lang="en">
+          <head>
+            <meta charset="utf-8">
+      
+            <title>Thank You for contacting TreatGlobe</title>
+            <meta name="description" content="TreatGlobe Enquiry Form">
+            <meta name="author" content="SitePoint">
+          <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+            <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+          </head>
+      
+          <body>
+            <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';"></div>
+                  <div class="container" style="margin-left: 20px;margin-right: 20px;">
+                  <h2>Hey ${firstName}, </h2>
+                  <p>Your query is noted!<br/> Our team will contact you as soon as possible</p>
+                  <div style="font-size: 16px;">
+                  <br>
+                  </div>
+                  </div>
+          </body>
+          </html>`,
+    };
+    transporter.sendMail(mailOptions);
 
     //google drive
     const authClient = await authorize();
