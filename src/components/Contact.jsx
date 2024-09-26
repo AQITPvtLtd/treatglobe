6869fed1aee49f031aa456a1757b4e4ccc5046d2
Palak <98663148+palak-7@users.noreply.github.com>
2024-09-26 15:07:12 +0530

    file size limitation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,18 @@ const ContactForm = () => {
       alert("Medical Report can't be empty!");
       return;
     }
+    if (file.size > 500 * 1024) {
+      // Check if file size exceeds 500KB
+      alert("File size must not exceed 500KB!");
+      return;
+    }
     if (!identityFile) {
       alert("Passport field can't be empty!");
+      return;
+    }
+    if (identityFile.size > 500 * 1024) {
+      // Check if file size exceeds 500KB
+      alert("File size must not exceed 500KB!");
       return;
     }
     fileData.append("myFile", file);
