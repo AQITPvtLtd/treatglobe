9be03403826dd53bc3f6bfc64d4ed29ef8d53baa
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-21 15:12:49 +0530

    change pass

--- src/helper/db.js
+++ src/helper/db.js
@@ -11,7 +11,7 @@ import mysql from "mysql2";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Tekbooster@1991",
+  password: "asdfasdf",
   database: "treatglobe",
   waitForConnections: true,
   connectionLimit: 10, // Maximum number of connections in the pool
