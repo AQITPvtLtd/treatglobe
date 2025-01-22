5266e2deae5bc85aa61f391a01a20623baa131fd
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-22 16:10:17 +0530

    add new file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['www.facebook.com'], // Facebook domain ko allow kiya gaya hai
+    },
+};
+
+module.exports = nextConfig;
--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -10,7 +10,6 @@ import { dir } from "i18next";
 import initTranslations from "../i18n";
 import TranslationsProvider from "@/components/TranslationsProvider";
 import Script from "next/script"; // Correct Next.js Script import
-import Image from "next/image";
 
 const i18nNamespaces = [
   "home",
@@ -55,21 +54,21 @@ export default async function RootLayout({ children, params: { locale } }) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '926766189586191');
-              fbq('track', 'PageView');
-            `,
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '926766189586191');
+                fbq('track', 'PageView');
+              `,
           }}
         />
         <noscript>
-          <Image
+          <img
             height="1"
             width="1"
             style={{ display: "none" }}
